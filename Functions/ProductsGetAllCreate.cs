c899324fff509973fe778ec18bf49c3e4771dbe9
Patrik Rydén <Patrik Rydn@DESKTOP-LOBA5VG>
2024-05-08 14:28:17 +0200

    Updated to Rest-API standard

--- Functions/ProductsGetAllCreate.cs
+++ Functions/ProductsGetAllCreate.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
@@ -12,35 +13,98 @@ namespace PatrikFunctionApp.Functions
     public class ProductsGetAllCreate
     {
         private readonly AppDbContext _context;
-
-        public ProductsGetAllCreate(AppDbContext context)
+        private readonly ILogger<ProductsGetAllCreate> _logger;
+        public ProductsGetAllCreate(AppDbContext context, ILogger<ProductsGetAllCreate> logger)
         {
             _context = context;
+            _logger = logger;
         }
 
-        [Function("ProductsGetAllCreate")]
-        public async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "products")] HttpRequest req)
+        [Function("GetAllProducts")]
+        public async Task<IActionResult> GetAllProducts(
+            [HttpTrigger(AuthorizationLevel.Anonymous,  "get", Route = "products")] HttpRequest req)
         {
-            string functionKey = req.Headers["x-functions-key"];
-            string expextedKey = Environment.GetEnvironmentVariable("SuperSecretKey");
+            
+            var products = await _context.Products.ToListAsync();
+            _logger.LogInformation("Get all products");
+            return new OkObjectResult(products);
+        }
 
-            if (functionKey != expextedKey)
+        [Function("CreateProduct")]
+        public async Task<IActionResult> CreateProduct(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "products")] HttpRequest req)
+        {
+            
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var product = JsonConvert.DeserializeObject<Product>(requestBody);
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+            _logger.LogInformation("Created product");
+            return new CreatedResult("/products", product);
+
+        }
+       
+        [Function("DeleteProduct")]
+        public async Task<IActionResult> DeleteProduct(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "delete", Route = "products/{id}")] HttpRequest req, Guid id)
+        {
+            _logger.LogInformation("Deletes a product");
+            var productToDelete = await _context.Products.FirstOrDefaultAsync(product => product.Id == id);
+       
+            if (productToDelete == null)
             {
-                return new UnauthorizedResult();
+                return new NotFoundResult();
             }
-
-            if (req.Method == HttpMethods.Post)
+            else
+            {
+                 _context.Products.Remove(productToDelete);
+                 await _context.SaveChangesAsync();
+                return new OkObjectResult("Item was deleted");
+            }
+       
+        }
+       
+        [Function("UpdateProduct")]
+        public async Task<IActionResult> UpdateProduct(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "products/{id}")] HttpRequest req, Guid id)
+        {
+          
+       
+           var prodcutToUpdate = await _context.Products.FirstOrDefaultAsync(product => product.Id == id);
+       
+            if (prodcutToUpdate == null)
+            {
+                return new NotFoundResult();
+            }
+            else
             {
                 string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-                var product = JsonConvert.DeserializeObject<Product>(requestBody);
-                _context.Products.Add(product);
+                var newProductInformation = JsonConvert.DeserializeObject<Product>(requestBody);
+                prodcutToUpdate.Name = newProductInformation.Name;
+                prodcutToUpdate.Price  = newProductInformation.Price;
+                _context.Products.Update(prodcutToUpdate);
                 await _context.SaveChangesAsync();
-                return new CreatedResult("/products", product);
+                return new OkObjectResult(prodcutToUpdate);
             }
-
-            var products = await _context.Products.ToListAsync();
-            return new OkObjectResult(products);
+        }
+       
+        [Function("GetProduct")]
+        public async Task<IActionResult> GetProduct(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "products/{id}")] HttpClient req, Guid id)
+        {
+            var selectedProduct = await _context.Products.FirstOrDefaultAsync(p => p.Id == id);
+            if (selectedProduct == null)
+            {
+                return new NotFoundResult();
+            }
+            else
+            {
+                return new OkObjectResult(selectedProduct);
+            }
+       
+       
+       
         }
     }
 }
--- Model/Product.cs
+++ Model/Product.cs
@@ -8,7 +8,7 @@ namespace PatrikFunctionApp.Model
 {
     public class Product
     {
-        public Guid Id { get; set; }
+        public Guid Id { get; set; } 
         public string Name { get; set; }
         public double Price { get; set; }
     }
